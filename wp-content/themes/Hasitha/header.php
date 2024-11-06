fb0905f21658e4a0ecfd183959d108b43e8c4791
Hasitha24 <hasithak002@gmail.com>
2024-11-06 13:03:23 +0530

    Added Bootstrap CSS and JS in functions.php

--- wp-content/themes/Hasitha/header.php
+++ wp-content/themes/Hasitha/header.php
@@ -13,7 +13,16 @@
     <title>Wordpress Theme</title>
     <?php wp_head();?>
 </head>
-<body <?php body_class('Hello-Hasi'); ?>>
+<body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+<!-- A function should be checked if it exists prior to use it.Use the below
+ method to check if wp_body_open() exists, so that all 
+ the uses who use lower wp versions can also use your code -->
+
+<?php
+
+if (function_exists('wp_body_open')){
+    wp_body_open();
+}
+ ?>
 <header>Header</header>
