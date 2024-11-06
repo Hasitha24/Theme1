789957ec14b209ceed7ce2c26f8d2125538ccde8
Hasitha24 <hasithak002@gmail.com>
2024-11-06 13:02:34 +0530

    Added bootstrap css and js in functions.php

--- wp-content/themes/Hasitha/functions.php
+++ wp-content/themes/Hasitha/functions.php
@@ -23,12 +23,21 @@ function hasitha_enqueue_scripts(){
 //wp_register_style is important at the times we have to use wp_enqueue conditionally.
 //eg. when we have to enqueue scripts only for a certain page. etc..So using the we_register_style initially is the best practise
 
+//Register Styles
 wp_register_style('style-css',get_stylesheet_uri(),[],filemtime(get_template_directory().'/style.css'),'all');
+wp_register_style('bootstrap-css',get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css',[], false ,'all');
+
+//Register Scripts
 wp_register_script( 'main-js',get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js' ), true);
+wp_register_script( 'bootstrap-js',get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false , true);
 
-
+//Enqueue Styles
 wp_enqueue_style('style-css');
+wp_enqueue_style('bootstrap-css');
+
+//Enqueue Scripts
 wp_enqueue_script('main-js');
+wp_enqueue_script('bootstrap-js');
 
 }
 add_action('wp_enqueue_scripts','hasitha_enqueue_scripts');
